e95aa60631f6a786e3bb51e167fe0f792039ef61
Crimsey <41775862+Crimsey@users.noreply.github.com>
2021-01-11 23:48:57 +0100

    welcomeactivity added

--- app/src/main/java/com/example/android_werble/WelcomeActivity.java
+++ app/src/main/java/com/example/android_werble/WelcomeActivity.java
@@ -1,4 +1,41 @@
 package com.example.android_werble;
 
-public class WelcomeActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.registerButton)
+    Button register;
+    @BindView(R.id.loginButton)
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 }
